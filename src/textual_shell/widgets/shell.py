d57a7834b39a8dc5706fa2f6acf857e8fffe45c1
Jason-Lawrence <thejasonlawrence1@gmail.com>
2025-01-01 10:17:28 -0500

    move suggestions to subpackage

--- src/textual_shell/widgets/shell.py
+++ src/textual_shell/widgets/shell.py
@@ -1,11 +1,16 @@
 from typing import Annotated
 
+from textual import events
 from textual.app import ComposeResult
 from textual.binding import Binding
 from textual.message import Message
 from textual.reactive import reactive
 from textual.widget import Widget
-from textual.widgets import Input, Label
+from textual.widgets import (
+    Input,
+    Label,
+    OptionList
+)
 
 
 
@@ -139,3 +144,83 @@ class Prompt(Widget):
         prompt_input.value = ''
         prompt_input.action_home()
         self.post_message(self.CommandEntered(event.value))
+        
+        
+class Suggestions(OptionList):
+    """Widget for displaying the suggestion for auto-completions as a pop-up."""
+    
+    class FocusChange(Message):
+        """
+        A message for when the prompt input 
+        has either gained or lost focus.
+        """
+        def __init__(self, is_focused: bool):
+            super().__init__()
+            self.is_focused = is_focused
+            
+            
+    class Cycle(Message):
+        """
+        Cycle the highlighted suggestion.
+        
+        Args:
+            next (str): The next suggestion.
+        """
+        def __init__(self, next: Annotated[str, 'The next suggestion.']):
+            super().__init__()
+            self.next = next
+
+
+    class Continue(Message):
+        """Select the current highlighted suggestion."""
+        pass
+    
+    class Hide(Message):
+        """Hide the suggestions."""
+        pass
+    
+    class Cancel(Message):
+        """Cancel the suggestion and undo the auto-completion."""
+        pass
+    
+    
+    BINDINGS = [
+        Binding('backspace', 'cancel_completion', 'Cancel Autocompletion'),
+        Binding('tab', 'cycle', 'Cycle autocompletion', priority=True),
+        Binding('space', 'continue', 'Select autocompletion'),
+        Binding('escape', 'hide', 'Hide autosuggestion')
+    ]
+
+    def on_focus(self, event: events.Focus) -> None:
+        """The Suggestion widget has gained focus."""
+        self.post_message(self.FocusChange(True))
+    
+    def on_blur(self, event: events.Blur) -> None:
+        """The Suggestion widget has lost focus."""
+        self.post_message(self.FocusChange(False))
+      
+    def action_cancel_completion(self) -> None:
+        """Cancel the auto-completion."""
+        self.highlighted = None
+        self.post_message(self.Cancel())
+      
+    def action_cycle(self) -> None:
+        """Cycle to the next completion."""
+        if self.option_count == 0:
+            return 
+        
+        next = self.highlighted + 1
+        if next >= self.option_count:
+            next = 0
+        
+        self.highlighted = next
+        suggestion = self.get_option_at_index(next).prompt
+        self.post_message(self.Cycle(suggestion))
+        
+    def action_continue(self) -> None:
+        """Select the autocompletion."""
+        self.post_message(self.Continue())
+        
+    def action_hide(self) -> None:
+        """Hide the Suggestions"""
+        self.post_message(self.Hide())
