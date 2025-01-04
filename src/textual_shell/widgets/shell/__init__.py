ca9a110ed93bb95b2ec2eb8789c4969fe145b799
Jason-Lawrence <thejasonlawrence1@gmail.com>
2025-01-04 13:20:09 -0500

    move to subpackage

---
+++ src/textual_shell/widgets/shell/__init__.py
@@ -0,0 +1,11 @@
+from .prompt import Prompt, PromptInput
+from .shell import Shell
+from .suggestions import Suggestions
+
+
+__all__ = [
+    'Prompt',
+    'PromptInput',
+    'Shell',
+    'Suggestions'
+]
