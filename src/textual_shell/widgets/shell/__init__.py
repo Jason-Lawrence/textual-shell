a6472ed1d96bb3d4f540feeef3619096d1efabfc
Jason-Lawrence <thejasonlawrence1@gmail.com>
2025-01-04 17:45:23 -0500

    Make base shell class and sync shell

--- src/textual_shell/widgets/shell/__init__.py
+++ src/textual_shell/widgets/shell/__init__.py
@@ -1,9 +1,11 @@
+from .base_shell import BaseShell
 from .prompt import Prompt, PromptInput
 from .shell import Shell
 from .suggestions import Suggestions
 
 
 __all__ = [
+    'BaseShell',
     'Prompt',
     'PromptInput',
     'Shell',
