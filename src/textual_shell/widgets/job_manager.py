0b3ecd5decd1a64f99e00521c3638df8736eeda1
Jason-Lawrence <thejasonlawrence1@gmail.com>
2025-01-07 13:52:19 -0500

    add docstrings

--- src/textual_shell/widgets/job_manager.py
+++ src/textual_shell/widgets/job_manager.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from textual.app import ComposeResult
 from textual.widget import Widget
 from textual.widgets import (
@@ -9,7 +11,7 @@ from ..job import Job
 
 
 class JobManager(Widget):
-    """"""
+    """Manage currently running jobs."""
     
     DEFAULT_CSS = """
     
@@ -25,16 +27,50 @@ class JobManager(Widget):
         table = self.query_one(DataTable)
         self.column_keys = table.add_columns('Jobs', 'Status')
         
-    def add_job(self, job: Job) -> None:
-        """"""
+    def add_job(
+        self,
+        job: Annotated[Job, 'The job to add.']
+    ) -> None:
+        """
+        Add a new job to the table and dictionary.
+        
+        Args:
+            job (Job): The job to add.
+        """
         self.job_list[job.id] = job
         table = self.query_one(DataTable)
         row = (job.id, job.status)
         table.add_row(*row, key=job.id)
         
-    def remove_job(self, job_id: str) -> None:
+    def remove_job(
+        self,
+        job_id: Annotated[str, 'The id of the job.']
+    ) -> None:
+        """
+        Remove a job from the table and dictionary.
+        
+        Args:
+            job_id (str): The id of the job.
+        """
         job = self.job_list.pop(job_id)
         table = self.query_one(DataTable)
         table.remove_row(job_id)
         
+    def update_job_status(
+        self,
+        job_id: Annotated[str, 'The id of the job'],
+        status: Job.Status
+    ) -> None:
+        """
+        Update the status of the job in the table.
         
+        Args:
+            job_id (str): The id of the job.
+            status (Job.Status): The jobs current status.
+        """
+        table = self.query_one(DataTable)
+        table.update_cell(
+            row_key=job_id,
+            column_key=self.column_keys[1],
+            value=status
+        )
